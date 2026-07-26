dd6b26a60910b48da44fb0ab1e5be8a4e811a06a
shahuNew <jobsmails18@gmail.com>
2026-07-26 18:10:55 +0530

    Adding persistent high score systems

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 var score = 0
 var time_left = 30
+var high_score = 0
 
+@onready var high_score_label = $HighScoreLabel
 @onready var restart_button = $RestartButton
 @onready var score_label = $ScoreLabel
 @onready var timer_label = $TimerLabel
@@ -11,6 +13,7 @@ var time_left = 30
 
 func _ready():
 	ball.ball_clicked.connect(_on_ball_clicked)
+	load_high_score()
 
 func _process(delta):
 	if game_timer.time_left > 0:
@@ -20,6 +23,11 @@ func _on_ball_clicked():
 	if game_timer.time_left > 0:
 		score += 1
 		score_label.text = "Score: " + str(score)
+		
+	if score > high_score:
+			high_score = score
+			high_score_label.text = "High Score: " + str(high_score)
+			save_high_score()	
 
 func _on_game_timer_timeout():
 	ball.game_over = true
@@ -32,3 +40,16 @@ func _on_game_timer_timeout():
 
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
+	
+func load_high_score():
+	if FileAccess.file_exists("user://highscore.save"):
+		var file = FileAccess.open("user://highscore.save", FileAccess.READ)
+		high_score = file.get_32()
+		file.close()
+
+	high_score_label.text = "High Score: " + str(high_score)	
+
+func save_high_score():
+	var file = FileAccess.open("user://highscore.save", FileAccess.WRITE)
+	file.store_32(high_score)
+	file.close()
