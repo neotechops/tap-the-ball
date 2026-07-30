49210eaae519b79e4de3d407f499f4f0044c287b
shahuNew <jobsmails18@gmail.com>
2026-07-30 22:28:55 +0530

    Add pause menu with resume restart and main menu

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var score = 0
 var time_left = 30
 var high_score = 0
+var is_paused = false
 
 @onready var high_score_label = $HighScoreLabel
 @onready var restart_button = $RestartButton
@@ -10,9 +11,19 @@ var high_score = 0
 @onready var timer_label = $TimerLabel
 @onready var ball = $Ball
 @onready var game_timer = $GameTimer
+@onready var pause_button = $PauseButton
+@onready var pause_panel = $PausePanel
+@onready var resume_button = $PausePanel/ButtonContainer/ResumeButton
+@onready var pause_restart_button = $PausePanel/ButtonContainer/RestartButton
+@onready var main_menu_button = $PausePanel/ButtonContainer/MainMenuButton
 
 func _ready():
 	ball.ball_clicked.connect(_on_ball_clicked)
+	
+	pause_button.pressed.connect(_on_pause_pressed)
+	resume_button.pressed.connect(_on_resume_pressed)
+	pause_restart_button.pressed.connect(_on_pause_restart_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_pressed)
 	load_high_score()
 
 func _process(_delta):
@@ -53,3 +64,26 @@ func save_high_score():
 	var file = FileAccess.open("user://highscore.save", FileAccess.WRITE)
 	file.store_32(high_score)
 	file.close()
+
+func _on_pause_pressed():
+	is_paused = true
+
+	pause_panel.visible = true
+	get_tree().paused = true
+
+
+func _on_resume_pressed():
+	is_paused = false
+
+	pause_panel.visible = false
+	get_tree().paused = false
+
+
+func _on_pause_restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
